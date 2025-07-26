ce04916f16c7e4915f28fce108626ae20e217087
Vinay Mingari <mingarivinay@gmail.com>
2025-07-26 10:50:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const loanRoutes = require('./loanRoutes');
 const app = express();
 app.use('/loan', loanRoutes);
 app.get('/', (req, res) => {
-  console.log('✅ GET request received');
+  console.log('GET request received');
   res.send('Hello Vinay, your Express server is alive!');
 });
 
 app.listen(3000, () => {
-  console.log('🚀 Server is running on http://localhost:3000');
+  console.log('Server is running on http://localhost:3000');
 });
