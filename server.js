28138a2b22229e756ebfe74c960bfe57edafb000
Vinay Mingari <mingarivinay@gmail.com>
2025-07-26 09:12:00 +0530

    Complete backend assignment with README

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const loanRoutes = require('./loanRoutes'); 
+const app = express();
+app.use('/loan', loanRoutes);
+app.get('/', (req, res) => {
+  console.log('✅ GET request received');
+  res.send('Hello Vinay, your Express server is alive!');
+});
+
+app.listen(3000, () => {
+  console.log('🚀 Server is running on http://localhost:3000');
+});
